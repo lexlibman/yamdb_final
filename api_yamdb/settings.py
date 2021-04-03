b100f5b28354b5a2457e7b9410a685fd58182cbd
Aleksei Libman <lexlibman@gmail.com>
2021-04-03 16:11:47 +0300

    last

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -96,6 +96,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+
 AUTH_USER_MODEL = 'api.User'
 
 REST_FRAMEWORK = {
